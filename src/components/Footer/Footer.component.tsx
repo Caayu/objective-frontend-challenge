c087e5028b5f1640ac703feef07f09a222d2e0dd
caayu <henrycay@outlook.com>
2021-02-26 15:58:20 -0300

    refactor: move Footer component to own file

---
+++ src/components/Footer/Footer.component.tsx
@@ -0,0 +1,21 @@
+import { Container, Pagination, PaginationButton, IconNavigation } from './styles'
+
+function FooterComponent () {
+  return (
+    <Container>
+      <Pagination>
+        <IconNavigation>arrow_back</IconNavigation>
+        <IconNavigation>arrow_back_ios</IconNavigation>
+        <PaginationButton>1</PaginationButton>
+        <PaginationButton active>2</PaginationButton>
+        <PaginationButton>3</PaginationButton>
+        <PaginationButton>4</PaginationButton>
+        <PaginationButton>5</PaginationButton>
+        <IconNavigation>arrow_forward_ios</IconNavigation>
+        <IconNavigation>arrow_forward</IconNavigation>
+      </Pagination>
+    </Container>
+  )
+}
+
+export default FooterComponent
--- src/views/CharactersView/CharactersView.view.tsx
+++ src/views/CharactersView/CharactersView.view.tsx
@@ -1,32 +1,15 @@
-import {
-  Container,
-  Footer,
-  Pagination,
-  PaginationButton,
-  IconNavigation
-} from './styles'
+import { Container } from './styles'
 
 import Header from '../../components/Header'
 import CharacterArea from '../../components/CharactersArea'
+import Footer from '../../components/Footer'
 
 const CharactersView = () => {
   return (
     <Container>
       <Header />
       <CharacterArea />
-      <Footer>
-        <Pagination>
-          <IconNavigation>arrow_back</IconNavigation>
-          <IconNavigation>arrow_back_ios</IconNavigation>
-          <PaginationButton>1</PaginationButton>
-          <PaginationButton active>2</PaginationButton>
-          <PaginationButton>3</PaginationButton>
-          <PaginationButton>4</PaginationButton>
-          <PaginationButton>5</PaginationButton>
-          <IconNavigation>arrow_forward_ios</IconNavigation>
-          <IconNavigation>arrow_forward</IconNavigation>
-        </Pagination>
-      </Footer>
+      <Footer />
     </Container>
   )
 }
