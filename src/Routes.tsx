eb82150103dad65a1e0d69568768c6afbb050444
caayu <henrycay@outlook.com>
2021-02-27 23:42:50 -0300

    refactor: remove context

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,13 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import GlobalContextProvider from './contexts/GlobalContext'
-
 import CharactersView from './views/CharactersView'
 
 export default function Routes () {
   return (
     <Router>
-      <GlobalContextProvider>
-        <Switch>
-          <Route component={CharactersView} exact path="/" />
-        </Switch>
-      </GlobalContextProvider>
+      <Switch>
+        <Route component={CharactersView} exact path="/" />
+      </Switch>
     </Router>
   )
 }
