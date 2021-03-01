a9fe0845ab9ad85becea396b58c50c8dee2480bf
caayu <henrycay@outlook.com>
2021-03-01 01:53:21 -0300

    refactor: make root path redirect to heros

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 
 import Heros from './views/Heros'
 import HeroDetail from './views/HeroDetail'
@@ -7,6 +7,9 @@ export default function Routes () {
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          <Redirect to="/heros" />
+        </Route>
         <Route component={Heros} exact path="/heros" />
         <Route component={HeroDetail} exact path="/heros/:id" />
       </Switch>
